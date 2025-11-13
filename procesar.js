e105fa7882d1b19bdf633a41bd84893ef0582c56
Agustt123 <agustintracheskyoficial@gmail.com>
2025-11-13 12:17:08 -0300

    sdada

--- procesar.js
+++ procesar.js
@@ -188,7 +188,9 @@ async function processWebhook(data2) {
       if (cachedSellers.length === 0 || !cachedSellers.includes(incomeuserid)) {
         try {
           const response = await axios.get(
-            "https://callbackml.lightdata.app/MLProcesar/get/"
+            // "https://callbackml.lightdata.app/MLProcesar/get/"
+            "https://whml.lightdata.app/sellersactivos/showAll/"
+
 
           );
 
